b83ad588d4aa2d38ab40e6c5776a72feda88ec3e
Chen Emerson <99339391+EmersonCh1458@users.noreply.github.com>
2022-05-14 18:30:47 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -50,7 +50,7 @@ const BLOG = {
       repo: ''
     },
     cusdisConfig: {
-      appId: '', // data-app-id
+      appId: '4a281f86-718b-4780-b90c-f74af16df4c3', // data-app-id
       host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
     }
